fb56061bf98eb5c2fd36f16229a30dc4ea537116
marinadumberg <marina.dumberg@inbox.lv>
2021-11-06 21:59:43 +0200

    modal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import Modal from "./modal/modal";
+import ModalExit from "./modal/modalExit";
 import "./App.css";
 
+
 function App() {
-  return <div className="App"></div>;
+  const [modalActive,setModalActive]=useState(true)
+  return <div className="App">
+
+    <Modal active={modalActive} setActive={setModalActive} />
+    <ModalExit/>
+  </div>;
+
 }
 
 export default App;
