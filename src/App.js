8f475e00e4ae146d7692b8a557eb468d7bd4da1b
ivanna777 <70724335+ivanna777@users.noreply.github.com>
2021-11-07 16:05:40 +0300

    murkup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Balance from "./Components/Balance/Balance";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Balance />
+  </div>;
 }
 
 export default App;
