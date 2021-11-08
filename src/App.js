44d5186e7394f985c60d6c8785ffe85d5c33c827
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2021-11-09 00:26:39 +0200

    ///

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import { useState } from "react";
 import Modal from "./modal/modal";
 import ModalExit from "./modal/modalExit";
+import Container from "./components/Container/Container";
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
 
-
 function App() {
-
-  const [modalActive,setModalActive]=useState(true)
+  const [modalActive, setModalActive] = useState(true);
   return (
     <div className="App">
-  <HomePage />
-    <Modal active={modalActive} setActive={setModalActive} />
-    <ModalExit/>
-  </div>);
-
-
+      <HomePage />
+      <Modal active={modalActive} setActive={setModalActive} />
+      <ModalExit />
+      <Container />
+    </div>
+  );
 }
-
 export default App;
