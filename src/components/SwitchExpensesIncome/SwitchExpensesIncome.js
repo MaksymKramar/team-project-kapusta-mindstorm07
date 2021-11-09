3de073e53618d2a9af89c75fdd44326e23c17b6c
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2021-11-09 15:44:36 +0200

    fix

--- src/components/SwitchExpensesIncome/SwitchExpensesIncome.js
+++ src/components/SwitchExpensesIncome/SwitchExpensesIncome.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Expenses from "../Expenses/Expenses";
 import Income from "../Income/Income";
-// import s from "./SwitchExpensesIncome.module.scss";
 
 export default function SwitchExpensesIncome() {
   return (
