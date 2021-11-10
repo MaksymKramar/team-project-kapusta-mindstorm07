03e4a2dcf550bb71ddd3b5d62e93ad1462db07c0
ivanna777 <70724335+ivanna777@users.noreply.github.com>
2021-11-11 00:38:07 +0300

    done

--- src/Components/ItemsCategories/ItemsCategories.js
+++ src/Components/ItemsCategories/ItemsCategories.js
@@ -1,20 +1,29 @@
+// import Button from '@mui/material/Button';
+import sprite from "../../images/sprite.svg";
 import styles from './ItemsCategories.module.css'
 
 function ItemsCategories() {
-  return <select className={styles["itemsCategoriesGroup"]}>
-            <option selected className={styles["itemCategory"]}>Категория товара</option>
-            <option className={styles["itemCategory"]}>Транспорт</option>
-            <option className={styles["itemCategory"]}>Продукты</option>
-            <option className={styles["itemCategory"]}>Здоровье</option>
-            <option className={styles["itemCategory"]}>Алкоголь</option>
-            <option className={styles["itemCategory"]}>Развлечения</option>
-            <option className={styles["itemCategory"]}>Всё для дома</option>
-            <option className={styles["itemCategory"]}>Техника</option>
-            <option className={styles["itemCategory"]}>Коммуналка, связь</option>
-            <option className={styles["itemCategory"]}>Спорт, хобби</option>
-            <option className={styles["itemCategory"]}>Образование</option>
-            <option className={styles["itemCategory"]}>Прочее</option>
-        </select>
+return <div className={styles["dropdown"]}>
+    <button className={styles["dropbtn"]}>Категория товара <svg width="12" height="20" className={styles["category-svg-down"]}>
+      <use href={sprite + "#down"}></use>
+    </svg>
+    <svg width="12" height="20" className={styles["category-svg-up"]}>
+      <use href={sprite + "#up"}></use>
+    </svg>
+    </button>
+  <ul className={styles["dropdown-content"]}>
+  <li className={styles["dropdown-content-a"]}>Транспорт</li>
+  <li className={styles["dropdown-content-a"]}>Продукты</li>
+  <li className={styles["dropdown-content-a"]}>Здоровье</li>
+  <li className={styles["dropdown-content-a"]}>Алкоголь</li>
+  <li className={styles["dropdown-content-a"]}>Развлечения</li>
+  <li className={styles["dropdown-content-a"]}>Всё для дома</li>
+  <li className={styles["dropdown-content-a"]}>Коммуналка, связь</li>
+  <li className={styles["dropdown-content-a"]}>Спорт, хобби</li>
+  <li className={styles["dropdown-content-a"]}>Образование</li>
+  <li className={styles["dropdown-content-a"]}>Прочее</li>
+  </ul>
+</div>
 }
 
 export default ItemsCategories;
