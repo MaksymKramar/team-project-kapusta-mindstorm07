d35b04236d6be8f5c3302df694088644b7e58214
ivanna777 <70724335+ivanna777@users.noreply.github.com>
2021-11-14 13:30:22 +0300

    message

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import { useState } from "react";
-import Modal from "./modal/modal";
-import ModalExit from "./modal/modalExit";
+// import { useState } from "react";
+// import Modal from "./modal/modal";
+// import ModalExit from "./modal/modalExit";
 
 import "./App.css";
-import DateCalendar from "./Components/Date/Date";
-import Reports from "./pages/Reports/Reports";
-import { useEffect } from "react";
-import { fetchCurrentUser } from "./redux/auth/auth-operation";
-import { useSelector, useDispatch } from "react-redux";
-import authSelector from "./redux/auth/auth-selector";
-import { Switch } from "react-router-dom";
-import PubliceRoute from "./routes/PublicRoute";
-// import PrivateRoute from './routes/PrivateRoute'
-import { Suspense, lazy } from "react";
-import Summary from "./components/Summary/summary";
-import Balance from "./components/Balance/Balance";
+import BackBtn from "./Components/BackBtn/BackBtn";
+// import DateCalendar from "./Components/Date/Date";
+// import Reports from "./pages/Reports/Reports";
+// import { useEffect } from "react";
+// import { fetchCurrentUser } from "./redux/auth/auth-operation";
+// import { useSelector, useDispatch } from "react-redux";
+// import authSelector from "./redux/auth/auth-selector";
+// import { Switch } from "react-router-dom";
+// import PubliceRoute from "./routes/PublicRoute";
+// // import PrivateRoute from './routes/PrivateRoute'
+// import { Suspense, lazy } from "react";
+// import Summary from "./components/Summary/summary";
+// import Balance from "./components/Balance/Balance";
 
-const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
-const SignUpPage = lazy(() => import("./pages/SignUpPage/SignUpPage"));
+// const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+// const SignUpPage = lazy(() => import("./pages/SignUpPage/SignUpPage"));
 
 function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchCurrentUser());
-  }, [dispatch]);
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   dispatch(fetchCurrentUser());
+  // }, [dispatch]);
 
-  const [modalActive, setModalActive] = useState(true);
+  // const [modalActive, setModalActive] = useState(true);
 
   return (
     <div className="App">
-      <Switch>
+      {/* <Switch>
         <Suspense fallback={null}>
           <PubliceRoute path="/" exact>
             {<HomePage />}
@@ -48,7 +49,8 @@ function App() {
       <ModalExit />
       <Balance />
       <Reports />
-      <DateCalendar />
+      <DateCalendar /> */}
+      <BackBtn />
     </div>
   );
 }
