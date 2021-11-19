a2bffdbf22d19e3e206a9ab0e3de34de140819d2
ivanna777 <70724335+ivanna777@users.noreply.github.com>
2021-11-19 21:11:38 +0300

    message

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-// import { useState } from "react";
-// import Modal from "./modal/modal";
-// import ModalExit from "./modal/modalExit";
+import { useState } from "react";
+import Modal from "./modal/modal";
+import ModalExit from "./modal/modalExit";
 
 import "./App.css";
-// import { useEffect } from "react";
-// import { fetchCurrentUser } from "./redux/auth/auth-operation";
-// import { useSelector, useDispatch } from "react-redux";
-// import authSelector from "./redux/auth/auth-selector";
-// import { Switch } from "react-router-dom";
-// import PubliceRoute from "./routes/PublicRoute";
-// // import PrivateRoute from './routes/PrivateRoute'
-// import { Suspense, lazy } from "react";
-// import Summary from "./components/Summary/summary";
-// import ReportPage from "./pages/ReportPage/ReportPage";
+import { useEffect } from "react";
+import { fetchCurrentUser } from "./redux/auth/auth-operation";
+import { useSelector, useDispatch } from "react-redux";
+import authSelector from "./redux/auth/auth-selector";
+import { Switch } from "react-router-dom";
+import PubliceRoute from "./routes/PublicRoute";
+// import PrivateRoute from './routes/PrivateRoute'
+import { Suspense, lazy } from "react";
+import Summary from "./components/Summary/summary";
+import ReportPage from "./pages/ReportPage/ReportPage";
 import Balance from "./components/Balance/Balance";
 
-// const LogInPage = lazy(() => import("./pages/LogInPage/LogInPage"));
-// const SignUpPage = lazy(() => import("./pages/SignUpPage/SignUpPage"));
+const LogInPage = lazy(() => import("./pages/LogInPage/LogInPage"));
+const SignUpPage = lazy(() => import("./pages/SignUpPage/SignUpPage"));
 
 function App() {
-  // const dispatch = useDispatch();
-  // useEffect(() => {
-  //   dispatch(fetchCurrentUser());
-  // }, [dispatch]);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCurrentUser());
+  }, [dispatch]);
 
-  // const [modalActive, setModalActive] = useState(false);
-  // const [modalExitActive, setModalExitActive] = useState(false);
+  const [modalActive, setModalActive] = useState(false);
+  const [modalExitActive, setModalExitActive] = useState(false);
 
   return (
     <div className="App">
-      {/* <Switch>
+      <Switch>
         <Suspense fallback={null}>
           <PubliceRoute path="/" exact>
             {<LogInPage setActive={setModalExitActive} />}
@@ -43,11 +43,11 @@ function App() {
             {<SignUpPage />}
           </PubliceRoute>
         </Suspense>
-      </Switch> */}
-      {/* <Summary />
+      </Switch>
+      <Summary />
       <Modal active={modalActive} setActive={setModalActive} />
       <ModalExit active={modalExitActive} setActive={setModalExitActive} />
-      <ReportPage /> */}
+      <ReportPage />
       <Balance />
     </div>
   );
