6cc91d1e2e4b2e2b0546f0fac5517b09a8d7315c
alinashveida <alinashveida@gmail.com>
2021-11-19 13:46:12 +0200

    правки

--- src/pages/LoadingPage/LoadingPage.jsx
+++ src/pages/LoadingPage/LoadingPage.jsx
@@ -4,12 +4,13 @@ import { useLocation } from "react-router-dom";
 import queryString from "query-string";
 
 export default function LoadingPage() {
-  //
-  const location = useLocation();
-  //   const useremail = new URLSearchParams(search).get('useremail')
-  //   console.log(useremail)
-  const { useremail } = queryString.parse(location.search);
-  console.log("queryParams :>> ", useremail);
+  const search = useLocation().search;
+  const useremail = new URLSearchParams(search).get("useremail");
+  console.log(useremail);
+
+  // const location = useLocation().search
+  // const { useremail } = queryString.parse(location.search);
+  // console.log( useremail);
 
   const dispatch = useDispatch();
   dispatch(authGoogle(useremail));
