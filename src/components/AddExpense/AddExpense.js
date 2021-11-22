729556e806452dfe845d1616774a171bd179d60a
alinashveida <alinashveida@gmail.com>
2021-11-22 18:02:15 +0200

    баланс обновляеться

--- src/components/AddExpense/AddExpense.js
+++ src/components/AddExpense/AddExpense.js
@@ -1,5 +1,5 @@
-import React, { useState} from "react";
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import * as operations from "../../redux/transactionAdd/transactionAdd-operations";
 import * as selectors from "../../redux/transactionAdd/transactionADD-selectors";
 // import {transactionAdd, getGategories} from '../../redux/transactionAdd/transactionAdd-operations'
@@ -11,46 +11,54 @@ import styles from "./AddExpense.module.css";
 import "react-datepicker/dist/react-datepicker.css";
 import DateCalendar from "../Date/Date";
 
+import { getBalance } from "../../redux/auth/auth-operation";
+
 export default function AddExpense() {
   const [startDate, setStartDate] = useState(new Date());
-  const [description, setDescription] = useState('');
+  const [description, setDescription] = useState("");
   const [sum, setSum] = useState(0);
-  const [category, setCategory] = useState('');
-  const [type, setType] = useState()
-  const [value, setValue] = useState('Категория товара');
+  const [category, setCategory] = useState("");
+  const [type, setType] = useState();
+  const [value, setValue] = useState("Категория товара");
 
   const dispatch = useDispatch();
 
-  const date = `${startDate.getDate()}.${startDate.getMonth()+1}.${startDate.getFullYear()}`
+  const date = `${startDate.getDate()}.${
+    startDate.getMonth() + 1
+  }.${startDate.getFullYear()}`;
   // console.log(date)
   // setDate(transDate)
-  
+
   // console.log(date)
   // setTransactionDate(date)
   // console.log(transactionDate)
-  
+
   const getCategoriesItem = () => {
     dispatch(operations.getGategories());
-  }
+  };
 
-  const categories = useSelector(selectors.getCategoriesAll)
-  const getDatas = useSelector(selectors.getData)
-  console.log(getDatas)
+  const categories = useSelector(selectors.getCategoriesAll);
+  const getDatas = useSelector(selectors.getData);
+  const getDescription = useSelector(selectors.getDescription);
+  console.log(getDatas);
+
+  // useEffect(() => {
+  //   dispatch(getBalance())
+  // }, [getDescription])
   // const catItem = categories.map(i=>i.title).reduce()
   const change = (e) => {
-    categories.map(i => {
-      if(i.title === e.target.textContent ) {
-        setCategory(i._id)
-        setType(i.type)
+    categories.map((i) => {
+      if (i.title === e.target.textContent) {
+        setCategory(i._id);
+        setType(i.type);
       }
-     })
-    
+    });
+
     // console.log(catItem)
-    setValue(e.target.textContent)
-    
-  }
-console.log(category)
-  const handleChange = e => {
+    setValue(e.target.textContent);
+  };
+  console.log(category);
+  const handleChange = (e) => {
     switch (e.target.name) {
       case "description":
         return setDescription(e.target.value);
@@ -59,33 +67,33 @@ console.log(category)
       default:
         return;
     }
-  }
-
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    const newTransaction = {date: getDatas, description, sum, type, category};
-    dispatch(operations.transactionAdd(newTransaction))
-  }
-
-  const clearBtn = () => {
-    setSum('');
-    setDescription('');
-    setCategory('');
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTransaction = { date: getDatas, description, sum, type, category };
+
+    dispatch(operations.transactionAdd(newTransaction));
+  };
+
+  const clearBtn = () => {
+    setSum("");
+    setDescription("");
+    setCategory("");
+  };
 
   return (
     <div className="container">
-    <DateCalendar />
+      <DateCalendar />
       <form className={styles.wrapper} onSubmit={handleSubmit}>
         <div className={styles.itemsWrapper}>
           <div className={styles.itemDiv}>
-            <input 
-            className={styles.itemInput} 
-            placeholder="Описание товара"
-            name="description"
-            onChange={handleChange}/>
+            <input
+              className={styles.itemInput}
+              placeholder="Описание товара"
+              name="description"
+              onChange={handleChange}
+            />
             {/* <ItemCategories /> */}
             {/* <div className={styles["dropdown"]}>
             <ul className={styles["dropdown-content"]} name="category" onClick={getCategoriesItem}>
@@ -97,27 +105,44 @@ console.log(category)
              </ul>
              </div> */}
             <div className={styles["dropdown"]}>
-      <button className={styles["dropbtn"]} onClick={getCategoriesItem}>
-        {value}
-        <svg width="12" height="20" className={styles["category-svg-down"]}>
-          <use href={sprite + "#down"}></use>
-        </svg>
-        <svg width="12" height="20" className={styles["category-svg-up"]}>
-          <use href={sprite + "#up"}></use>
-        </svg>
-      </button>
-      <ul className={styles["dropdown-content"]}>
-        {categories.map(category => (
-                  <li className={styles["dropdown-content-a"]} key={category._id} value={category._id} onClick={change}>{category.title}</li>
-                ))}</ul>
-    </div>
+              <button className={styles["dropbtn"]} onClick={getCategoriesItem}>
+                {value}
+                <svg
+                  width="12"
+                  height="20"
+                  className={styles["category-svg-down"]}
+                >
+                  <use href={sprite + "#down"}></use>
+                </svg>
+                <svg
+                  width="12"
+                  height="20"
+                  className={styles["category-svg-up"]}
+                >
+                  <use href={sprite + "#up"}></use>
+                </svg>
+              </button>
+              <ul className={styles["dropdown-content"]}>
+                {categories.map((category) => (
+                  <li
+                    className={styles["dropdown-content-a"]}
+                    key={category._id}
+                    value={category._id}
+                    onClick={change}
+                  >
+                    {category.title}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
           <div className={styles.amountDiv}>
-            <input 
-            className={styles.amountInput} 
-            placeholder="00.00 UAH"
-            name="sum"
-            onChange={handleChange} />
+            <input
+              className={styles.amountInput}
+              placeholder="00.00 UAH"
+              name="sum"
+              onChange={handleChange}
+            />
             <button className={styles.calculatorBtn}>
               <svg width="20" height="20" className={styles["report-svg"]}>
                 <use href={sprite + "#icon-calculator"}></use>
@@ -126,8 +151,12 @@ console.log(category)
           </div>
         </div>
         <div className={styles.btnsDiv}>
-          <button type="submit" className={styles.enterBtn}>Ввод</button>
-          <button type="button" className={styles.clearBtn} onClick={clearBtn}>Очистить</button>
+          <button type="submit" className={styles.enterBtn}>
+            Ввод
+          </button>
+          <button type="button" className={styles.clearBtn} onClick={clearBtn}>
+            Очистить
+          </button>
         </div>
       </form>
     </div>
--- src/redux/transactionAdd/transactionADD-selectors.js
+++ src/redux/transactionAdd/transactionADD-selectors.js
@@ -1,3 +1,5 @@
 export const getCategoriesAll = (state) => state.transactionAdd.categories;
 
 export const getData = (state) => state.transactionAdd.date;
+
+export const getDescription = (state) => state.transactionAdd.description;
