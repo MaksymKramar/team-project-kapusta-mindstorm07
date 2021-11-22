61363339b60cdb230fd5dc0ba6657df47a90c99f
Maksym Kramar <maxim.kramar77@gmail.com>
2021-11-22 04:13:22 +0100

    Сделал рендер таблицы с базы

--- src/redux/transactions/transactionsSelectors.js
+++ src/redux/transactions/transactionsSelectors.js
@@ -1,17 +1,8 @@
-import { createSelector } from "@reduxjs/toolkit";
+export const getTransactionsTrue = (state) => state.transactions.itemsTrue;
+export const getTransactionsFalse = (state) => state.transactions.itemsFalse;
 
-export const getTransactions = (state) => state.transactions.items;
 export const isLoadingValue = (state) => state.transactions.isLoading;
 export const getTransactionsTotalAmount = (state) =>
   state.transactions.totalAmount;
 export const getTransactionsSum = (state) => state.transactions.sum;
 export const getLoading = (state) => state.transactions.isLoading;
-
-export const getTransactionsList = createSelector(
-  [getTransactions],
-  (transactions) => {
-    console.log("transactions:", transactions);
-    const transactionsSort = [...transactions];
-    return transactionsSort.sort((a, b) => new Date(b.date) - new Date(a.date));
-  }
-);
