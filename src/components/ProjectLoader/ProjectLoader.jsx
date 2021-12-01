1e8ee70bef31977a83862ebeff9a3b3451bb4d4c
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2021-12-01 09:58:46 +0200

    add ProjectLoader and fix Spinner

--- src/components/ProjectLoader/ProjectLoader.jsx
+++ src/components/ProjectLoader/ProjectLoader.jsx
@@ -1,12 +1,12 @@
 import Loader from "react-loader-spinner";
-import s from "./Spinner.module.scss";
+import s from "./ProjectLoader.module.scss";
 
-export default function Spinner({ width, height }) {
+export default function ProjectLoader({ width, height }) {
   return (
     <div className={s.wrapper}>
       <Loader
-        type="ThreeDots"
-        color="#f8f8f8"
+        type="Audio"
+        color="var(--accent-color)"
         height={height}
         width={width}
         timeout={0}
